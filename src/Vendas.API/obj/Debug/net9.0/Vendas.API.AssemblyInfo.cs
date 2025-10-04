a188697d6ca0ec6ea911b456304903252748609a
AdrianoBSantana <adrianobs44@gmail.com>
2025-10-04 20:19:14 -0300

    feat(vendas): implementa modelos de Pedido

--- src/Vendas.API/obj/Debug/net9.0/Vendas.API.AssemblyInfo.cs
+++ src/Vendas.API/obj/Debug/net9.0/Vendas.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vendas.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf5b9a18d02b7ea0a2941993d3b14d613ef9ccf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9006e270f31cefed9d788000ee66984e874d86e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vendas.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vendas.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
